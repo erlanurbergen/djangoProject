f8ffd2c4ec0de2db7b041cdbc9100b2e1852f540
erlankarabaliyev <erlanurbergen@gmail.com>
2023-02-21 11:53:42 +0600

    lecture6 Basket

--- store/storeProducts/views.py
+++ store/storeProducts/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 
+from .models import Product, ProductCategory, Basket
+import sys
+sys.path.append('/Users/erlankarabaliyev/Desktop/spring kbtu/django/djangoProject/store/users')
+from users .models import User
 
-from .models import Product, ProductCategory
 # Create your views here.
 
 def index(request):
@@ -20,4 +23,27 @@ def products(request):
         'categories' : ProductCategory.objects.all(),
     }
     return render(request, 'storeProducts/products.html', context)
+
+
+def basket_add(request, product_id):
+    product = Product.objects.get(id=product_id)
+    baskets = Basket.objects.filter(user=request.user, products=product)
+
+    if not baskets.exists():
+        Basket.objects.create(user=request.user, products=product, quantity=1)
+    else:
+        basket = baskets.first()
+        basket.quantity += 1
+        basket.save()
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
+
+def basket_remove(request, basket_id):
+    basket = Basket.objects.get(id=basket_id)
+    basket.delete()
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
+
+
+
 
--- store/users/forms.py
+++ store/users/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 from django import forms
 
 from .models import User
@@ -33,7 +33,7 @@ class UserRegisterForm(UserCreationForm):
         'placeholder' : 'Введите имя пользователя'
     }))
 
-    # email = forms.CharField(widget=forms.EmailField(attrs={
+    # email = forms.EmailField(widget=forms.EmailField(attrs={
     #     'class' : 'form-control py-4',
     #     'placeholder' : 'Введите почту'
     # }))
@@ -50,3 +50,28 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
+
+
+class UserProfileForm(UserChangeForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class' : 'form-control py-4'
+    }))
+
+    last_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class' : 'form-control py-4',
+    }))
+
+    image = forms.ImageField(widget=forms.FileInput (attrs={
+        'class' : 'custom-file-input'
+    }), required=False)
+
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class' : 'form-control py-4', 'readonly': True
+    }))
+
+    # email = forms.CharField(widget=forms.TextInput(attrs={
+    #     'class' : 'form-control py-4', 'readonly': True
+    # }))
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'image', 'username', 'email')
